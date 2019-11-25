1c4a08e03219670b19814976032bf24d3bfb230b
Dieyson.Rander@hotmail.com <Dieyson.Rander@hotmail.com>
2019-11-25 20:15:15 -0300

    Inicio do template de login

---
+++ VirtualShop.Web/ClientApp/src/app/produto/produto.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-produto",
+  template: ""
+})
+export class ProdutoComponent {
+
+  public id: number;
+  public nome: string;
+  public preco: number;
+
+}
