b0d5166a125a82f9346fcc05d47534743bf0da7a
Dieyson.Rander@hotmail.com <Dieyson.Rander@hotmail.com>
2019-11-26 21:25:24 -0300

    Teste de validação do Login com o sessionStorage

--- VirtualShop.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ VirtualShop.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,16 +1,28 @@
 import { Component } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls:["./login.component.css"]
 })
+
 export class LoginComponent {
 
   public usuario;
 
-  constructor() {
+  constructor(private router: Router) {
+
     this.usuario = new Usuario();
+
+
+  }
+
+  entrar() {
+    if (this.usuario.email == "teste@teste.com" && this.usuario.senha == "abc123") {
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate(['/']);
+    }
   }
 }
