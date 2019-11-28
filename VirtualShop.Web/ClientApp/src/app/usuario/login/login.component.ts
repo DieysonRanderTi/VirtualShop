d3b2a6a350446570c98d27cc91af6c400e94f0ac
Dieyson.Rander@hotmail.com <Dieyson.Rander@hotmail.com>
2019-11-27 22:42:46 -0300

    Inicio controller Usuario

--- VirtualShop.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ VirtualShop.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
+import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
 
 @Component({
   selector: "app-login",
@@ -8,21 +9,34 @@ import { Router } from "@angular/router";
   styleUrls:["./login.component.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+    
 
   public usuario;
+  public returnUrl: string;
 
-  constructor(private router: Router) {
-
-    this.usuario = new Usuario();
-
+  constructor(private router: Router, private activateRouter: ActivatedRoute, private usuarioServico: UsuarioServico) {
 
   }
 
+  ngOnInit(): void {
+    this.usuario = new Usuario();
+    this.returnUrl = this.activateRouter.snapshot.queryParams['returnUrl'];
+  }
+
   entrar() {
-    if (this.usuario.email == "teste@teste.com" && this.usuario.senha == "abc123") {
-      sessionStorage.setItem("usuario-autenticado", "1");
-      this.router.navigate(['/']);
-    }
+
+    this.usuarioServico.verificarUsuario(this.usuario)
+      .subscribe(data => {
+
+      },
+        err => {
+
+        }
+     );
+
+    //if (this.usuario.email == "teste@teste.com" && this.usuario.senha == "abc123") {
+    //  sessionStorage.setItem("usuario-autenticado", "1");
+    //  this.router.navigate([this.returnUrl]);} 
   }
 }
---
+++ VirtualShop.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VitualShop.Dominio.Entidades;
+
+namespace VirtualShop.Web.Controllers
+{
+    [Route("api/[Controller]")]
+    public class UsuarioController: Controller
+    {
+        [HttpPost]
+        public ActionResult Post()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+                
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+
+            }
+        }
+
+        [HttpPost("VerificarUsuario")]
+        public ActionResult VerificarUsuario([FromBody] Usuario usuario)
+        {
+            try
+            {
+                if (usuario.Email == "teste@teste.com" && usuario.Senha == "abc123")
+                {
+                  return Ok(usuario);
+                }
+
+                return BadRequest("Usuário ou senha inválido");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+
+            }
+        }
+    }
+}
